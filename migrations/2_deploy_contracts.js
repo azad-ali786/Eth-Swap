808f78022a198085af97fc92518b0b2da53e0c80
azad-ali786 <azad_ug@cse.nits.ac.in>
2021-08-20 01:02:16 +0530

    Built token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
+const Token = artifacts.require("Token");
 const EthSwap = artifacts.require("EthSwap");
 
+
 module.exports = function(deployer) {
+  //deploy token to blockchain
+  deployer.deploy(Token);
+  // deploy ethswap to blockchain
   deployer.deploy(EthSwap);
 };
